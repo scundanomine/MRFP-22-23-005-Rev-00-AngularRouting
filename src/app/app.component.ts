0899dad955f74cc23625ad1b8ce4c49225e50eb3
unknown <scundanomine21@gmail.com>
2022-10-05 20:06:39 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'MRFP-22-23-005-Rev-00-AngularRouting';
+}
