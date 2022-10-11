36fe2474140c3a8d9b8cad61a9ed852988cd8104
unknown <scundanomine21@gmail.com>
2022-10-11 22:38:23 +0530

    w

--- src/app/department-details/department-details.component.ts
+++ src/app/department-details/department-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-department-details',
@@ -8,16 +9,50 @@ import { Component, OnInit } from '@angular/core';
 export class DepartmentDetailsComponent implements OnInit {
 
   departments = [
-    {id:1, name:"Angular"},
-    {id:2, name:"Node"},
-    {id:3, name:"MongoDB"},
-    {id:4, name:"Ruby"},
-    {id:5, name:"Bootstrap"}
+    { id: 1, name: "Angular" },
+    { id: 2, name: "Node" },
+    { id: 3, name: "MongoDB" },
+    { id: 4, name: "Ruby" },
+    { id: 5, name: "Bootstrap" }
   ]
-  
-  constructor() { }
+
+  // b:string="";
+  userId = "";
+  detail = "";
+  flags = false;
+
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.userId = this.route.snapshot.paramMap.get('id') || ""
+    let uid = parseInt(this.userId);
+
+    let flag = false
+    for (let i = 0; i < this.departments.length; i++) {
+      if (this.departments[i].id == uid) {
+        this.detail=this.departments[i].name;
+        flag = true;
+        this.flags=flag;
+        break;
+      }
+    }
+    if (flag == false)
+      this.flags = false;
   }
+
+  //   getUserById(uid:number) {
+  //     console.log(uid)
+  //     let flag=false;
+  //     for(let i=0;i<this.departments.length;i++){
+  //         if(this.departments[i].id==uid) {
+  //             // res.send("UserName:"+userData[i].userName)
+  //             flag=true;
+  //             break;
+  //         }
+  //     }
+  //     if(flag==false)
+  //         this.b ='Requested Data not found';
+  // }
 
 }
